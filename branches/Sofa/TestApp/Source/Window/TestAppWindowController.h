f287c03b936730fdcdf09a26786e5d1290359612
catshive <catshive@c1f3293d-aa4e-0410-9a15-579adf76fb40>
2009-02-05 14:06:32 +0000

    Making Sofa branch

---
+++ branches/Sofa/TestApp/Source/Window/TestAppWindowController.h
@@ -0,0 +1,16 @@
+//
+//  TestAppWindowController.h
+//  KTUIKit
+//
+//  Created by Cathy Shive on 11/23/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <KTUIKit/KTUIKit.h>
+
+@interface TestAppWindowController : KTWindowController 
+{
+}
+
+@end
